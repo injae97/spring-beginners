d4b5219df3670a58959a588fee5104666e6807d4
InJeKim97 <injaekim97@naver.com>
2022-10-25 00:44:30 +0900

    feat/main/InJekim97 : API (JSON TYPE)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,5 +27,30 @@ public class HelloController {
     public String HelloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+
+    /**
+     * @param name
+     * @return JSON : {"name":"spring!!!"}
+     */
+    @GetMapping("hello-api")
+    @ResponseBody // HTTP-BODY에 문자 내용 직접 반환 (페이지 검사시 데이터만 있음)
+    public Hello HelloAPI(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // object로 들어오면 HttpMessageConverter(JsonConverter)에 의해 JSON
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
