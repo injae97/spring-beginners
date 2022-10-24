e09500c2ede3090ffc429d2ebe4685fadc35403e
InJeKim97 <injaekim97@naver.com>
2022-10-25 00:00:04 +0900

    feat/main/InJekim97 : MVC (@RequestParam)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,14 +3,28 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
 
 
     @GetMapping("hello") // GET 
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+
+    /**
+     * @param name (url:path?name=value)
+     * @return hello-template
+     */
+    @GetMapping("hello-mvc")
+    public String HelloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
