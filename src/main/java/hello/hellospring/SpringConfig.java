f53cf2dec288de023b1a8ce9d4e9356a307c6995
InJeKim97 <injaekim97@naver.com>
2022-10-27 22:59:25 +0900

    feat/main/InJekim97 : JDBC Connect

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,37 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 직접 자바 코드로 Bean 등록
+ * 상황에 따라 구현 클래스를 변경할 경우 (DB)
+ */
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
