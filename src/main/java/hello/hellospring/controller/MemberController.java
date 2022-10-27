c3c299deb08184cfa511bb9cfdd17f36f5541d5e
InJeKim97 <injaekim97@naver.com>
2022-10-27 19:04:52 +0900

    feat/main/InJekim97 : Component Scan Registration (Annotation)

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+
+    @Autowired  /* 의존 관계 주입(Dependency Injection)  */
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
     /* 외부에서 넣어주도록 수정 - DI 가능하게 변경 (Generate > Constructor) */
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
