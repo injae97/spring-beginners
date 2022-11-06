f6eff324374533074ee422ba19612d273ac0009e
InJeKim97 <injaekim97@naver.com>
2022-11-06 20:53:45 +0900

    feat/main/InJekim97 : AOP (모든 메소드의 호출 시간을 측정)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -24,39 +24,3 @@ public class SpringConfig {
         return new MemberService(memberRepository);    // 스프링 데이터 JPA
     }
 }
-
-
-
-
-
-
-
-
-    /*
-    private final DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-    */
-
-    /*
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    */
-
-    /*
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();                    // Memory에 데이터 저장 (스프링 재시작 시 데이터 삭제)
-        // return new JdbcMemberRepository(dataSource);            // JDBC를 통해 데이터 저장 (엄청 긴 코드)
-        // return new JdbcTemplateMemberRepository(dataSource);    // JdbcTemplate를 통해 데이터 저장 (엄청 간략해진 코드)
-        return new JpaMemberRepository(em);                        // JPA를 통해 데이터 저장
-    }
-    */
-
